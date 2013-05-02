c1c20f673bef5e156b603fa1fce98bc650ecf8a4
Nathan Strauser <nathan.strauser@gmail.com>
2013-05-02 14:45:48 -0400

    syntax

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ StateMachine.js pacakged for meteor
 
 See https://github.com/jakesgordon/javascript-state-machine
 and http://codeincomplete.com/posts/2013/1/26/javascript_state_machine_v2_2_0/
+
+exposes StateMachine as a global variable
--- package.js
+++ package.js
@@ -3,5 +3,5 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-    api.add_files('lib/javascript-state-machine/state-machine.js', ['client', 'server']);
+    api.add_files(['lib/javascript-state-machine/state-machine.js'], ['client', 'server']);
 });
