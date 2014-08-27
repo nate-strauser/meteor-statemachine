a2820b83c29da1dbf33fb5f6f6d2648ff5066cb5
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 11:21:32 -0400

    meteor 0.9.0

--- package.js
+++ package.js
@@ -1,7 +1,11 @@
 Package.describe({
-    summary: "Javascript State Machine packaged for meteor"
+	name:"natestrauser:statemachine",
+    summary: "Javascript State Machine packaged for meteor",
+  version: "0.1.1",
+  git: "https://github.com/nate-strauser/meteor-statemachine.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.add_files(['lib/javascript-state-machine/state-machine.js'], ['client', 'server']);
 });
